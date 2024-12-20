3b4e0e1bd2c1b7d8891c3f4a637c7c509c727d98
hjkim <hjkim99@hanyang.ac.kr>
2024-12-20 15:19:58 +0900

    csc32 final ver.

---
+++ scalesim/run_csc_32.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ARRAY=$1
+SIZE=$2
+
+# Config File Path
+TOPOLOGY_FILE="../topologies/mlperf/Resnet50.csv"
+CONFIG_FILE="../configs/csc/bw32/eyeriss_os_${ARRAY}_${SIZE}.cfg"
+LOG_DIR="../test_runs/csc32"
+INPUT_TYPE="conv"
+
+#Run ScaleSim
+python scale.py \
+    -t $TOPOLOGY_FILE \
+    -c $CONFIG_FILE \
+    -p $LOG_DIR \
+    -i $INPUT_TYPE \
